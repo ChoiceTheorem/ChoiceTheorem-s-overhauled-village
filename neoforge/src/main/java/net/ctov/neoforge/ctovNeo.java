05a21ec42f5e221d2e469d193400fdb8872d981f
ChoiceTheorem <pongpanot.icey@gmail.com>
2024-07-22 20:30:12 +0700

    Neoforge port

--- neoforge/src/main/java/net/ctov/neoforge/ctovNeo.java
+++ neoforge/src/main/java/net/ctov/neoforge/ctovNeo.java
@@ -2,13 +2,9 @@ package net.ctov.neoforge;
 
 import net.ctov.ctov;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(ctov.MOD_ID)
 public class ctovNeo {
     public ctovNeo() {
-        // Submit our event bus to let architectury register our content on the right time
-            // Initialize logic here
-        var modBus = FMLJavaModLoadingContext.get().getModEventBus();
         ctov.init();
     }
 }
