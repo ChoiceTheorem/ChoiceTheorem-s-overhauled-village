ec1fb96320485c6776c4fa937ef1c3574eac05c1
Apollo <102649729+Apollounknowndev@users.noreply.github.com>
2026-03-06 01:47:56 -0500

    Fix compat with Lithostitched 1.6

--- common/src/main/java/net/choicetheorem/ctov/CTOV.java
+++ common/src/main/java/net/choicetheorem/ctov/CTOV.java
@@ -1,24 +1,21 @@
 package net.choicetheorem.ctov;
 
+import dev.worldgen.lithostitched.api.event.AddWorldgenModifiersEvent;
 import net.choicetheorem.ctov.utils.CTOVStructureHelper;
-import net.choicetheorem.ctov.utils.TextUtils;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.Identifier;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructureSets;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
 
 import java.util.List;
 
 import static net.choicetheorem.ctov.platform.CTOVConfigHelper.*;
-import static net.choicetheorem.ctov.utils.TextUtils.res;
 
 public class CTOV {
 	public static final String MOD_ID = "ctov";
-	
-	private static final Identifier VANILLA_VILLAGES_SET_ID = TextUtils.res("minecraft", "villages");
-	private static final Identifier VANILLA_OUTPOSTS_SET_ID = TextUtils.res("minecraft", "pillager_outposts");
 	
 	public static void init() {
 		//System.out.println(ctovExpectPlatform.getConfigDirectory().toAbsolutePath().normalize().toString());
@@ -33,56 +30,34 @@ public class CTOV {
 	}
 	
 	public static void registerStructures(MinecraftServer server) {
-		HolderLookup.Provider registryProvider = server.registryAccess();
-		HolderLookup<StructureSet> structureSetLookup = registryProvider.lookupOrThrow(Registries.STRUCTURE_SET);
-		HolderLookup<Structure> structureLookup = registryProvider.lookupOrThrow(Registries.STRUCTURE);
-		
-		if (generatePillagerOutpost()) {
-			registerVariants(
-				enabledPillagerOutposts(),"pillager_outpost_", VANILLA_OUTPOSTS_SET_ID,
-				outpostWeight(), structureSetLookup, structureLookup
-			);
-		}
-		
-		if (generateSmallVillage()) {
-			registerVariants(
-				enabledVillages(), "small/village_", VANILLA_VILLAGES_SET_ID,
-				smallVillageWeight(), structureSetLookup, structureLookup
-			);
-		}
-		
-		if (generateMediumVillage()) {
-			registerVariants(
-				enabledVillages(), "medium/village_", VANILLA_VILLAGES_SET_ID,
-				mediumVillageWeight(), structureSetLookup, structureLookup
-			);
-		}
-		
-		if (generateLargeVillage()) {
-			registerVariants(
-				enabledVillages(), "large/village_", VANILLA_VILLAGES_SET_ID,
-				largeVillageWeight(), structureSetLookup, structureLookup
-			);
-		}
-	}
-	
-	private static void registerVariants(
-		List<? extends String> variants,
-		String structurePathPrefix,
-		Identifier targetStructureSetId,
-		int weight,
-		HolderLookup<StructureSet> structureSetLookup,
-		HolderLookup<Structure> structureLookup
-	) {
-		for (String variant : variants) {
-			Identifier structureId = res(structurePathPrefix + variant);
-			CTOVStructureHelper.addToStructureSet(
-				structureSetLookup,
-				structureLookup,
-				targetStructureSetId,
-				structureId,
-				weight
-			);
-		}
+		AddWorldgenModifiersEvent.EVENT.register((registries, consumer) -> {
+			Holder.Reference<StructureSet> outpostSet = registries.lookupOrThrow(Registries.STRUCTURE_SET).getOrThrow(BuiltinStructureSets.PILLAGER_OUTPOSTS);
+			Holder.Reference<StructureSet> villageSet = registries.lookupOrThrow(Registries.STRUCTURE_SET).getOrThrow(BuiltinStructureSets.VILLAGES);
+			HolderLookup.RegistryLookup<Structure> registry = registries.lookupOrThrow(Registries.STRUCTURE);
+			
+			if (generatePillagerOutpost()) {
+				for (String variant : enabledPillagerOutposts()) {
+					CTOVStructureHelper.addModifier(consumer, registry, outpostSet, "pillager_outpost_" + variant, outpostWeight());
+				}
+			}
+			
+			if (generateSmallVillage()) {
+				for (String variant : enabledVillages()) {
+					CTOVStructureHelper.addModifier(consumer, registry, villageSet, "small/village_" + variant, smallVillageWeight());
+				}
+			}
+			
+			if (generateMediumVillage()) {
+				for (String variant : enabledVillages()) {
+					CTOVStructureHelper.addModifier(consumer, registry, villageSet, "medium/village_" + variant, mediumVillageWeight());
+				}
+			}
+			
+			if (generateLargeVillage()) {
+				for (String variant : enabledVillages()) {
+					CTOVStructureHelper.addModifier(consumer, registry, villageSet, "large/village_" + variant, largeVillageWeight());
+				}
+			}
+		});
 	}
 }
--- common/src/main/java/net/choicetheorem/ctov/utils/CTOVStructureHelper.java
+++ common/src/main/java/net/choicetheorem/ctov/utils/CTOVStructureHelper.java
@@ -1,44 +1,22 @@
 package net.choicetheorem.ctov.utils;
 
-import dev.worldgen.lithostitched.worldgen.modifier.AddStructureSetEntriesModifier;
+import dev.worldgen.lithostitched.api.worldgen.modifier.WorldgenModifier;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
-import net.minecraft.world.level.levelgen.structure.StructureSet.StructureSelectionEntry;
 
-import java.util.List;
+import java.util.function.BiConsumer;
 
-public final class CTOVStructureHelper {
-	
-	private static final int DEFAULT_PRIORITY = 1;
-	
-	private CTOVStructureHelper() {
-		// Utility class; no instances.
-	}
-	
-	public static void addToStructureSet(
-		HolderLookup<StructureSet> structureSetLookup,
-		HolderLookup<Structure> structureLookup,
-		Identifier structureSetId,
-		Identifier structureId,
-		int weight
-	) {
-		final Holder.Reference<StructureSet> structureSetHolder =
-			structureSetLookup.getOrThrow(ResourceKey.create(Registries.STRUCTURE_SET, structureSetId));
-		final Holder.Reference<Structure> structureHolder =
-			structureLookup.getOrThrow(ResourceKey.create(Registries.STRUCTURE, structureId));
-		
-		final HolderSet<StructureSet> targetStructureSet = HolderSet.direct(structureSetHolder);
-		final StructureSelectionEntry newEntry = new StructureSelectionEntry(structureHolder, weight);
-		
-		final AddStructureSetEntriesModifier modifier =
-			new AddStructureSetEntriesModifier(DEFAULT_PRIORITY, targetStructureSet, List.of(newEntry));
-		
-		modifier.applyModifier();
+public class CTOVStructureHelper {
+	public static void addModifier(BiConsumer<Identifier, WorldgenModifier> consumer, HolderLookup.RegistryLookup<Structure> registry, Holder.Reference<StructureSet> outpostSet, String name, int weight) {
+		ResourceKey<Structure> key = ResourceKey.create(Registries.STRUCTURE, TextUtils.res(name));
+		consumer.accept(
+			key.identifier(),
+			WorldgenModifier.builder().addStructureSetEntries(outpostSet, new StructureSet.StructureSelectionEntry(registry.getOrThrow(key), weight))
+		);
 	}
 }
